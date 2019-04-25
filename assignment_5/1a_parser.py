5b9e5b0da1795d4913c16ca3bc9bad7046f78b14
tbullmann <bullmann.torsten@gmail.com>
2019-04-25 20:54:44 +0200

    Comments for the BLOSSUM parser

--- assignment_5/1a_parser.py
+++ assignment_5/1a_parser.py
@@ -1,27 +1,30 @@
 filename = "BLOSUM62.txt"
 
-def txt_substitionmatrix(filename):
+def txt_to_substition_matrix(filename):
     file = open(filename, "r")
     matrix = dict()
     for line in file:
         if line.startswith("#") or line.startswith("*"):
-            pass               # Ignore line with comments and * aminoacid
-        elif line.startswith(" "):
-            column_names = []  # Initialize list with aminoacids in column name line
-            # Replace carriage return and newline as well as * aminoacid
-            line = line.replace("\r","").replace("\n","").replace("*", "")
+            pass                                         # Ignore line with comments and the last line starting with *
+        elif line.startswith(" "):                       # This is the line containing column names in one letter code
+            column_names = []
+            line = line.replace("\r","").replace("\n","").replace("*", "")       # Delete carriage return, new line, *
             for character in line:
                 if character != " ":
-                    column_names.append(character)
-        else:   # else line starts with aminoacid
-            # positive numbers with leading +, so we can simply split lines by space
+                    column_names.append(character)                            # Add single letter code to column names
+        else:                                    # One letter code followed by scores for the each pair of amino acids
+            # Every column is 3 character wide, negative scores have one " ", whereas positive scores have two "  ".
+            # By replacing "  " with " +" each value has exactly one leading space by which we can split the line into
+            # two character strings containing the scores.
             splitted_line = line.replace("  ", " +").split(" ")
             for i in range(0, len(column_names)):
-                # key is AA in line, AA in column; value in list at index
-                matrix[(splitted_line[0], column_names[i])]= int(splitted_line[i+1])
+                # Key is a tuple of the one letter code at the beginning of the line and the one letter code in the
+                # column name. Note that the column names are indexed from 0, whereas in the splitted line they are
+                # indexed from 1, therefore we add 1 to the index of the column names
+                matrix[(splitted_line[0], column_names[i])]= int(splitted_line[i + 1])
     file.close()
     return matrix
 
-blossum62 = txt_substitionmatrix(filename)
+blossum62 = txt_to_substition_matrix(filename)
 print(blossum62)
 
